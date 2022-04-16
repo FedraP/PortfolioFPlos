6b4227facedc68a64753b21440a3656fd0dac56f
FedraP <fedra.plos@gmail.com>
2022-04-16 15:01:01 -0300

    Agregado seccion Datos Personales

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { EncabezadoComponent } from './components/encabezado/encabezado.component';
 import { BarraPpalComponent } from './components/barra-ppal/barra-ppal.component';
 import { BotonIngresarComponent } from './components/boton-ingresar/boton-ingresar.component';
+import { DatosPersonalesComponent } from './components/datos-personales/datos-personales.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EncabezadoComponent,
     BarraPpalComponent,
-    BotonIngresarComponent
+    BotonIngresarComponent,
+    DatosPersonalesComponent
   ],
   imports: [
     BrowserModule
